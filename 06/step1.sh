3afb2bfc13e7937955689c94bd9a4c016e1da25b
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-12-14 15:07:12 +0000

    Added subscripts for 06

---
+++ 06/step1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+source /etc/os-release
+echo "This is a $ID System"
+valid_packages=""
+
+if [ "$#" -lt 1 ]; then
+  echo "At least one package name is required."
+  exit 1
+fi
+
+for package_name in "$@" ; do
+  if ! apt-cache search "^$package_name$" | grep -q "$package_name"; then
+    valid-package
+    echo "The package $package_name is not available."
+    continue
+  fi
+
+  if [ "$ID" = 'ubuntu' ] ; then
+    pkgcache_file="/var/cache/apt/pkgcache.bin"
+    current_time=$(date +%s)
+    updated_time=$(stat -c%Y "$pkgcache_file")
+    time_diff=$((current_time - updated_time))
+    #The threshold is set to one day or 86400 seconds
+    threshold=86400
+    if [ "$time_diff" -le "$threshold" ]; then
+       echo "Package cache has been updated in the last 24 hours."
+       echo "Running the command sudo apt install -y $package_name"
+    else
+       echo "Package cache has not been updated in the last 24 hours."
+       echo "Running the command sudo apt update"
+       echo "Running the command sudo apt install -y $package_name"
+    fi
+  fi
+done
