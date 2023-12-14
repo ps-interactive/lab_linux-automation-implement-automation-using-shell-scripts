bc310e71d34f50d3c7566c72c295065b54a88fcf
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2023-12-14 14:35:25 +0000

    Added subscripts for 02-05

---
+++ 03/step5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+source /etc/os-release
+echo "This is a $ID System"
+
+if [ "$ID" = 'ubuntu' ] ; then
+  pkgcache_file="/var/cache/apt/pkgcache.bin"
+  # Returns the time as seconds after the Linux Epoch 1/1/1970
+  current_time=$(date +%s)
+  # Returns the last update time in number of seconds after the Epoch
+  updated_time=$(stat -c%Y "$pkgcache_file")
+  # The time_diff shows the number of seconds since the last update
+  time_diff=$((current_time - updated_time))
+  # The threshold is set to one day or 86400 seconds
+  threshold=86400
+  # If the time)diff is less than a day (86400s) then it is up to date, else it is not up to date
+  if [ "$time_diff" -le "$threshold" ]; then
+     echo "Package cache has been updated in the last 24 hours."
+  else
+     echo "Package cache has not been updated in the last 24 hours."
+  fi
+fi
---
+++ 05/step3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ "$#" -lt 1 ]; then
+  echo "At least one package name is required."
+  exit 1
+fi
+
+for package_name in "$@"
+  if ! apt-cache search "^$package_name$" | grep -q "$package_name"; then
+    echo "The package $package_name is not available."
+    exit 2
+  fi
+
+  source /etc/os-release
+  echo "This is a $ID System"
+
+  if [ "$ID" = 'ubuntu' ] ; then
+    pkgcache_file="/var/cache/apt/pkgcache.bin"
+    current_time=$(date +%s)
+    updated_time=$(stat -c%Y "$pkgcache_file")
+    time_diff=$((current_time - updated_time))
+    #The threshold is set to one day or 86400 seconds
+    threshold=86400
+    # If within the threshold we can install the package without updating the cache
+    if [ "$time_diff" -le "$threshold" ]; then
+       echo "Package cache has been updated in the last 24 hours."
+       echo "Running the command sudo apt install -y $package_name"
+    # Else we update the cache and install the package
+    else
+       echo "Package cache has not been updated in the last 24 hours."
+       echo "Running the command sudo apt update"
+       echo "Running the command sudo apt install -y $package_name"
+    fi
+  fi
+done
